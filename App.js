006dc0e9396812ac251e9b681210ddf35fd59dde
unknown <admin@pnvl104.pn.com.vn>
2022-10-14 17:14:30 +0700

    update

--- App.js
+++ App.js
@@ -3,29 +3,35 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const FirstScreen = () => {
-  const [flexDirection, setflexDirection] = useState("column");
-
+function FirstScreen() {
+  const [selectedValue, setSelectedValue] = useState('ltr');
+  const values = ['ltr', 'rtl'];
   return (
-    <PreviewLayout
-      label="flexDirection"
-      values={["column", "row", "row-reverse", "column-reverse"]}
-      selectedValue={flexDirection}
-      setSelectedValue={setflexDirection}
-    >
-      <View
-        style={[styles.box, { backgroundColor: "powderblue" }]}
-      />
-      <View
-        style={[styles.box, { backgroundColor: "skyblue" }]}
-      />
-      <View
-        style={[styles.box, { backgroundColor: "steelblue" }]}
-      />
-    </PreviewLayout>
+    <View style={{padding: 10, flex: 1}}>
+      <View style={styles.row}>
+        {values.map(value => (
+          <TouchableOpacity
+            key={value}
+            onPress={() => setSelectedValue(value)}
+            style={[styles.button, selectedValue === value && styles.selected]}>
+            <Text
+              style={[
+                styles.buttonLabel,
+                selectedValue === value && styles.selectedLabel,
+              ]}>
+              {value}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View style={[styles.container, {direction: selectedValue}]}>
+        <View style={{width: 50, height: 50, backgroundColor: '#EE2C38'}} />
+        <View style={{width: 50, height: 50, backgroundColor: '#FAA030'}} />
+        <View style={{width: 50, height: 50, backgroundColor: '#32B76C'}} />
+      </View>
+    </View>
   );
 };
-FirstScreen
 function SecondScreen() {
   const [position, setPosition] = useState('relative');
   const values = ['relative', 'absolute'];
@@ -104,13 +110,13 @@ const JustifyContentBasics = () => {
       setSelectedValue={setJustifyContent}
     >
       <View
-        style={[styles.box, { backgroundColor: "powderblue" }]}
+        style={[styles.box, { backgroundColor: "black" }]}
       />
       <View
-        style={[styles.box, { backgroundColor: "skyblue" }]}
+        style={[styles.box, { backgroundColor: "red" }]}
       />
       <View
-        style={[styles.box, { backgroundColor: "steelblue" }]}
+        style={[styles.box, { backgroundColor: "green" }]}
       />
     </PreviewLayout>
   );
